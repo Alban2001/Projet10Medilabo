e5a54817b4766286eb6c6737ed715d308750c2a3
Alban Voiriot <alban.voiriot@gmail.com>
2026-05-12 14:46:02 +0200

    create and initiate database

---
+++ APIDatabaseAuthenticationMediLabo/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIDatabaseAuthenticationMediLabo.Models
+{
+    public class User : IdentityUser
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public virtual ICollection<RendezVous>? ListeRendezVous { get; set; }
+    }
+}
